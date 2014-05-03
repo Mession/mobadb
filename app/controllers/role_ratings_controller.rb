0aab603de6fa8dd45abca2205118ade5eae03c58
Mession <messionyk@gmail.com>
2014-05-03 05:06:02 +0300

    implemented removing role ratings

--- app/controllers/role_ratings_controller.rb
+++ app/controllers/role_ratings_controller.rb
@@ -48,7 +48,7 @@ class RoleRatingsController < ApplicationController
   def destroy
     @rolerating.destroy
     respond_to do |format|
-      format.html { redirect_to role_ratingss_url }
+      format.html { redirect_to :back }
       format.json { head :no_content }
     end
   end

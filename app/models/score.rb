4cafd1d444f75dff2dacf211ab28202fe4a4ad5e
Eemi Haukkala <eemi.haukkala@gmail.com>
2014-05-05 00:08:46 +0300

    color visualisation for ratings

--- app/models/score.rb
+++ app/models/score.rb
@@ -6,4 +6,22 @@ class Score < ActiveRecord::Base
 
   # muokkaa alla rajoja, jos skaala vaihtuu
   validates :value, numericality: { greater_than_or_equal_to: 1, less_than_or_equal_to: 5, only_integer: true }
+
+
+  # viimehetkella keksitty hassu pikku lisa arvojen visualisointiin
+  def color_hex_code
+  	if self.value == 1
+  		return "#FF0000"
+  	elsif self.value == 2
+  		return "#FF8000"
+  	elsif self.value == 3
+  		return "#FFF000"
+  	elsif self.value == 4
+  		return "#80FF00"
+  	elsif self.value == 5
+  		return "#10FF00"
+  	end
+  	return ""
+  end
+
 end
